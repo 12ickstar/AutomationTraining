7813806c4f40170c6fa196b7cf3c3405bf129e23
Ricky Cabriles <rcabriles@expertcollege.com>
2022-10-07 11:03:12 +0800

    initial commit

---
+++ AutomationTraining/Training.E2E/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Training.E2E
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
